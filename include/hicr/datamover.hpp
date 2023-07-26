ea3c3b3627e0434824d891e985394b904fed0ad3
Sergio Miguel Martin <sergio.miguel.martin@huawei.com>
2023-07-26 12:24:55 +0000

    Pseudo-code update of the data motion API.

--- include/hicr/datamover.hpp
+++ include/hicr/datamover.hpp
@@ -1,4 +1,3 @@
-
 /*
  * Copyright Huawei Technologies Switzerland AG
  * All rights reserved.
@@ -8,23 +7,46 @@ namespace HiCR {
 
 /**
  * Encapsulates a memory region that is either the source or destination of a
- * call to HiCR::memcpy.
+ * call to put/get operations.
  *
- * There are a limited number of memory slots supported by any system.
+ * There might be limited number of memory slots supported by a given backend.
  */
 class MemorySlot {
- public:
-  /**
-   * Constructs a memory slot.
-   *
-   * @param[in] addr  The start address of the memory region to be registered.
-   * @param[in] size  The size of the memory region to be registered.
-   * @param[in] local Whether this is a local or global registration.
-   *
-   * Global registrations must be done collectively by all localities.
-   */
-  MemorySlot(void * addr, const size_t size, const bool local);
+  ...
 };
+
+/**
+ * This is a specialization of the Resource class in HiCR, meant to express a 
+ * hardware memory element within a hierarchy (E.g., Cache, RAM, HBM, Device)
+ *
+ * A more refined specification of this object and how it is discovered will be
+ * provided later with the first draft of the Machine Model API.
+ */
+class MemoryResource
+{
+ ...
+ 
+ /**
+ * Allocates a new memory slot within the memory resource.
+ *
+ * @param[in] addr  The start address of the memory region to be registered.
+ * @param[in] size  The size of the memory region to be registered.
+ */
+ MemorySlot MemoryResource::allocateMemorySlot(const size_t size);
+ 
+ /**
+ * Creates a memory slot within the memory resource given an existing allocation
+ *
+ * @param[in] addr  The start address of the memory region to be registered.
+ * @param[in] size  The size of the memory region to be registered.
+ */
+ MemorySlot MemoryResource::createMemorySlot(void* addr, const size_t size);
+ 
+  /**
+ * This operation will resolve the release of the allocated memory space which this slot holds.
+ */
+ void MemoryResource::freeMemorySlot();
+}
 
 /**
  * A message tag.
@@ -43,26 +65,14 @@ class TagSlot {
    *
    * There are a limited set of tags exposed by the system.
    *
-   * @param[in] name The name of the tag.
+   * @param[in] uuid A universally unique numerical tag
    */
-  TagSlot(const std::string& name);
+  TagSlot(const uint128_t uuid);
 }
 
 /**
- * A set of localities.
- */
-class LocalitySet {
- public:
-  /**
-   * Iterates over a set of locality IDs and adds them to the set.
-   */
-  template< typename ContainerIterator >
-  LocalitySet( ContainerIterator& start, ContainerIterator& end );
-}
-
-/**
- * Performs an asynchronous memory copy from within a source area, to within a
- * destination area.
+ * Instructs the backend to perform an asynchronous memory copy from
+ * within a source area, to within a destination area. 
  *
  * @param[in] source       The source memory region
  * @param[in] src_locality The locality of the source memory region
@@ -85,9 +95,9 @@ class LocalitySet {
  * \note For blocking semantics, simply immediately follow this call to memcpy
  *       with a call to HiCR::fence().
  */
-void memcpy(
- const MemorySlot& source, const size_t src_locality, const size_t src_offset,
+void Backend::memcpy(
  MemorySlot& destination, const size_t dst_locality, const size_t dst_offset,
+ const MemorySlot& source, const size_t src_locality, const size_t src_offset,
  const size_t size,
  const TagSlot& tag
 );
@@ -112,12 +122,11 @@ void fence(const TagSlot& tag);
  * @param[in] tag      The tag of the memory copies to fence for
  * @param[in] msgs_in  How many messages are incoming to this locality
  * @param[in] msgs_out How many messages are outgoing from this locality
- * @param[in] sources  A set of localities from which we have incoming memory
- *                     copy requests
+ * @param[in] sources  A set of memory resources (localities) from which 
+ *                     we have incoming memory copy requests
  */
 void fence(
  const TagSlot& tag,
  const size_t msgs_in, const size_t msgs_out,
- const LocalitySet& sources
+ const set<MemoryResource*>& sources
 );
-
