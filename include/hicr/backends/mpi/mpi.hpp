60041d3d6b9661b6b42371ef84d9485cf1cf1af6
Sergio Martin <sergio.miguel.martin@huawei.com>
2023-09-26 03:42:09 -0400

    Adding puts and gets#

--- include/hicr/backends/mpi/mpi.hpp
+++ include/hicr/backends/mpi/mpi.hpp
@@ -53,6 +53,22 @@ class MPI final : public Backend
   private:
 
   /**
+   * Struct to hold relevant memory slot information
+   */
+  struct globalMPISlot_t
+  {
+   /**
+    * Remembers the MPI of which this memory is local
+    */
+   int rank;
+
+   /**
+    * Stores the MPI window to use with this slot
+    */
+   MPI_Win* window;
+  };
+
+  /**
    * Default MPI communicator to use for this backend
    */
   const MPI_Comm _comm;
@@ -70,7 +86,7 @@ class MPI final : public Backend
   /**
    * Map of global slot id and MPI windows
    */
-  std::map<memorySlotId_t, MPI_Win*> _globalMemorySlotMPIWindowMap;
+  std::map<memorySlotId_t, globalMPISlot_t> _globalMemorySlotMPIWindowMap;
 
   /**
    * This function returns the available allocatable size in the current system RAM
@@ -108,6 +124,59 @@ class MPI final : public Backend
 
   __USED__ inline void memcpyImpl(memorySlotId_t destination, const size_t dst_offset, const memorySlotId_t source, const size_t src_offset, const size_t size) override
   {
+   // Sanity checks
+   if (_globalMemorySlotMPIWindowMap.contains(source)      == false) HICR_THROW_LOGIC("Trying to use MPI backend to with a source slot(%lu) that has not been registered as global.", source);
+   if (_globalMemorySlotMPIWindowMap.contains(destination) == false) HICR_THROW_LOGIC("Trying to use MPI backend to with a destination slot(%lu) that has not been registered as global.", destination);
+
+   // Checking whether source and/or remote are remote
+   bool isSourceRemote      = _globalMemorySlotMPIWindowMap[source].rank      != _rank;
+   bool isDestinationRemote = _globalMemorySlotMPIWindowMap[destination].rank != _rank;
+
+   // MPI backend should be used for local<->remote communications to determine whether Put or get should be used. This can be relaxed in the future, but for now we keep this in mind
+   if (isSourceRemote == false && isDestinationRemote == false) HICR_THROW_LOGIC("Trying to use MPI backend perform a local to local copy between slots (%lu -> %lu)", source, destination);
+   if (isSourceRemote == true  && isDestinationRemote == true)  HICR_THROW_LOGIC("Trying to use MPI backend perform a remote to remote copy between slots (%lu -> %lu)", source, destination);
+
+   // Perform a get if the source is remote and destination is local
+   if (isSourceRemote == true && isDestinationRemote == false)
+   {
+    // Calculating source pointer (with offset)
+    auto destinationPointer = (void*) (((uint8_t*)_memorySlotMap.at(destination).pointer) + dst_offset);
+
+    // Executing the get operation
+    auto status = MPI_Get(
+      destinationPointer,
+      size,
+      MPI_BYTE,
+      _globalMemorySlotMPIWindowMap[source].rank,
+      src_offset,
+      size,
+      MPI_BYTE,
+      *_globalMemorySlotMPIWindowMap[destination].window);
+
+    // Checking execution status
+    if (status != MPI_SUCCESS) HICR_THROW_RUNTIME("Failed to run MPI_Get (Slots %lu -> %lu)", source, destination);
+   }
+
+   // Perform a put if source is local and destination is remote
+   if (isSourceRemote == false && isDestinationRemote == true)
+   {
+    // Calculating source pointer (with offset)
+    auto sourcePointer = (void*) (((uint8_t*)_memorySlotMap.at(destination).pointer) + src_offset);
+
+    // Executing the get operation
+    auto status = MPI_Put(
+      sourcePointer,
+      size,
+      MPI_BYTE,
+      _globalMemorySlotMPIWindowMap[destination].rank,
+      dst_offset,
+      size,
+      MPI_BYTE,
+      *_globalMemorySlotMPIWindowMap[source].window);
+
+    // Checking execution status
+    if (status != MPI_SUCCESS) HICR_THROW_RUNTIME("Failed to run MPI_Put (Slots %lu -> %lu)", source, destination);
+   }
   }
 
   /**
@@ -194,7 +263,6 @@ class MPI final : public Backend
    // Calculating number of global slots
    int globalSlotCount = 0;
    for (const auto count : perProcessSlotCount) globalSlotCount += count;
-   printf("GlobalSlotCount: %u\n", globalSlotCount);
 
    // Allocating storage for local and global memory slot sizes
    std::vector<size_t> localSlotSizes(localSlotCount);
@@ -222,7 +290,7 @@ class MPI final : public Backend
    for (size_t i = 0; i < globalSlotPointers.size(); i++) globalSlotPointers[i] = globalSlotProcessId[i] == _rank ? _memorySlotMap.at(localPointerPos++).pointer : NULL;
 
    // Debug info
-   for (size_t i = 0; i < globalSlotSizes.size(); i++) printf("Rank: %u, GlobalSlot %lu, Size: %lu, LocalPtr: 0x%lX, %s\n", _rank, i, globalSlotSizes[i], (uint64_t)globalSlotPointers[i], globalSlotProcessId[i] == _rank ? "x" : "");
+   //for (size_t i = 0; i < globalSlotSizes.size(); i++) printf("Rank: %u, GlobalSlot %lu, Size: %lu, LocalPtr: 0x%lX, %s\n", _rank, i, globalSlotSizes[i], (uint64_t)globalSlotPointers[i], globalSlotProcessId[i] == _rank ? "x" : "");
 
    // Now creating global slots and their MPI windows
    for (size_t i = 0; i < globalSlotProcessId.size(); i++)
@@ -235,7 +303,7 @@ class MPI final : public Backend
       globalSlotSizes[i]);
 
     // Creating new entry in the MPI Window map
-    _globalMemorySlotMPIWindowMap[globalSlotId] = new MPI_Win;
+    _globalMemorySlotMPIWindowMap[globalSlotId] = globalMPISlot_t { .rank = globalSlotProcessId[i], .window = new MPI_Win };
 
     // Creating MPI window
     int status = MPI_Win_create(
@@ -244,7 +312,7 @@ class MPI final : public Backend
       1,
       MPI_INFO_NULL,
       _comm,
-      _globalMemorySlotMPIWindowMap[globalSlotId]);
+      _globalMemorySlotMPIWindowMap[globalSlotId].window);
 
     if (status != MPI_SUCCESS) HICR_THROW_RUNTIME("Failed to create MPI window on exchange global memory slots.");
    }
