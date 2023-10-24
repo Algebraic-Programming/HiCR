d53a991e42994889653ff6d9e726cec40e2194d0
Sergio Martin <sergio.miguel-martin@huawei.com>
2023-10-24 10:59:10 +0200

    Updating ascend container

--- .build-tools/containers/buildenv/run.sh
+++ .build-tools/containers/buildenv/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 1 ]]; then
-   echo "Usage: $0 <name>"
-   exit 1
+if [[ $# -ne 2 ]]; then
+ echo "Usage: $0 <name> <arch>"
+ exit 1
 fi
-folder=${1}
 
-docker run --name hicr -it "registry.gitlab.huaweirc.ch/zrc-von-neumann-lab/runtime-system-innovations/hicr/${folder}:latest" bash 
+folder=${1}
+arch=${2}
+
+docker run --name hicr -it "registry.gitlab.huaweirc.ch/zrc-von-neumann-lab/runtime-system-innovations/hicr/${folder}-${arch}:latest" bash 
--- .build-tools/containers/run.sh
+++ .build-tools/containers/run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 1 ]]; then
-   echo "Usage: $0 <name>"
-   exit 1
+if [[ $# -ne 2 ]]; then
+ echo "Usage: $0 <name> <arch>"
+ exit 1
 fi
+
 folder=${1}
+arch=${2}
 
 build_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-$build_dir/$folder/run.sh $folder
+$build_dir/$folder/run.sh ${folder} ${arch}
