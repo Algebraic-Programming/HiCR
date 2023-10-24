a9655f2d6509311e6bcd2478efc4a9f2c5762a2c
Luca Terracciano <luca.terraciano@huawei.com>
2023-10-24 19:06:17 +0200

    add automatic arch selection

--- .build-tools/containers/run.sh
+++ .build-tools/containers/run.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 2 ]]; then
- echo "Usage: $0 <name> <arch>"
- exit 1
+if command -v arch &>/dev/null; then
+   target_arch=$(arch)
+
+   if [ $target_arch == "aarch64" ]; then
+      target_arch="arm64v8"
+   else
+      target_arch="x86_64"
+   fi
+else
+   if [[ $# -ne 2 ]]; then
+      echo "arch not installed. Please provice manually the target architecture. Usage: $0 <name> <arch>"
+      exit 1
+   else
+      arch=${2}
+   fi
 fi
 
 folder=${1}
-arch=${2}
+echo "Running $folder for arch $target_arch"
 
 build_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-$build_dir/$folder/run.sh ${folder} ${arch}
+$build_dir/$folder/run.sh ${folder} ${target_arch}
