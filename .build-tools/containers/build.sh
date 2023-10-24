0e2fa345a0ebd574245e4ff0baacc673cbbf6c2d
Sergio Martin <sergio.miguel.martin@huawei.com>
2023-10-24 02:06:12 -0400

    Adding architecture parameter to docker image builder

--- .build-tools/containers/build.sh
+++ .build-tools/containers/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ $# -ne 1 ]]; then
-   echo "Usage: $0 <name>"
+if [[ $# -ne 2 ]]; then
+   echo "Usage: $0 <name> <arch>"
    exit 1
 fi
 folder=${1}
+arch=${2}
 
-docker build -t "registry.gitlab.huaweirc.ch/zrc-von-neumann-lab/runtime-system-innovations/hicr/${folder}:latest" ${folder}
+docker build -t "registry.gitlab.huaweirc.ch/zrc-von-neumann-lab/runtime-system-innovations/hicr/${folder}-${arch}:latest" --build-arg ARCH=${arch} ${folder} 
