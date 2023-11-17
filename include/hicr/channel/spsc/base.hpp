b6ce2b63ae4d4bc0af674ac7e8a68d59a8a538ed
Sergio Martin <sergio.miguel.martin@huawei.com>
2023-11-17 08:51:10 +0100

    Using circular buffer for spsc

--- include/hicr/channel/spsc/base.hpp
+++ include/hicr/channel/spsc/base.hpp
@@ -15,6 +15,7 @@
 
 #include <hicr/common/definitions.hpp>
 #include <hicr/common/exceptions.hpp>
+#include <hicr/common/circularBuffer.hpp>
 #include <hicr/backends/memoryManager.hpp>
 #include <hicr/memorySlot.hpp>
 
@@ -33,68 +34,9 @@ namespace SPSC
  * It exposes the functionality to be expected for a channel
  *
  */
-class Base
+class Base : public common::CircularBuffer
 {
   public:
-
-  /**
-   * @returns The capacity of the channel.
-   *
-   * This is a one-sided blocking call that need not be made collectively.
-   *
-   * This function when called on a valid channel instance will never fail.
-   */
-  __USED__ inline size_t getCapacity() const noexcept
-  {
-    return _capacity;
-  }
-
-  /**
-   * Returns the current channel depth.
-   *
-   * If the current channel is a consumer, it corresponds to how many tokens
-   * may yet be consumed. If the current channel is a producer, it corresponds
-   * the channel capacity minus the returned value equals how many tokens may
-   * still be pushed.
-   *
-   * \note This is not a thread-safe call
-   *
-   * This is a getter function that should complete in \f$ \Theta(1) \f$ time.
-   *
-   * @returns The number of tokens in this channel.
-   *
-   * This function when called on a valid channel instance will never fail.
-   */
-  __USED__ inline size_t getDepth() const noexcept
-  {
-    return _depth;
-  }
-
-  /**
-   * This function can be used to quickly check whether the channel is full.
-   *
-   * It affects the internal state of the channel because it detects any updates in the internal state of the buffers
-   *
-   * \returns true, if the buffer is full
-   * \returns false, if the buffer is not full
-   */
-  __USED__ inline bool isFull() const noexcept
-  {
-    return _depth == _capacity;
-  }
-
-  /**
-   * This function can be used to quickly check whether the channel is empty.
-   *
-   * It does not affects the internal state of the channel
-   *
-   * \returns true, if the buffer is empty
-   * \returns false, if the buffer is not empty
-   */
-  __USED__ inline bool isEmpty() const noexcept
-  {
-    return _depth == 0;
-  }
 
   /**
    * @returns The size of the tokens in this channel.
@@ -138,13 +80,18 @@ class Base
           MemorySlot *const tokenBuffer,
           MemorySlot *const coordinationBuffer,
           const size_t tokenSize,
-          const size_t capacity) : _memoryManager(memoryManager),
-                                   _tokenBuffer(tokenBuffer),
-                                   _coordinationBuffer(coordinationBuffer),
-                                   // Registering a slot for the local variable specifiying the nuber of popped tokens, to transmit it to the producer
-                                   _poppedTokensSlot(_memoryManager->registerLocalMemorySlot(&_poppedTokens, sizeof(size_t))),
-                                   _tokenSize(tokenSize),
-                                   _capacity(capacity)
+          const size_t capacity) :
+           common::CircularBuffer
+            (
+             capacity,
+             &_depth,
+             &_tail
+            ),
+           _memoryManager(memoryManager),
+           _tokenBuffer(tokenBuffer),
+           _coordinationBuffer(coordinationBuffer),
+           _poppedTokensSlot(_memoryManager->registerLocalMemorySlot(&_poppedTokens, sizeof(size_t))),
+           _tokenSize(tokenSize)
   {
     if (_tokenSize == 0) HICR_THROW_LOGIC("Attempting to create a channel with token size 0.\n");
     if (_capacity == 0) HICR_THROW_LOGIC("Attempting to create a channel with zero capacity \n");
@@ -154,38 +101,6 @@ class Base
   {
     // Unregistering memory slot corresponding to popped token count
     _memoryManager->deregisterLocalMemorySlot(_poppedTokensSlot);
-  }
-
-  /**
-   * @returns The current position of the buffer head
-   *
-   * Returns the position of the buffer head to set as offset for sending/receiving operations.
-   *
-   * This is a one-sided blocking call that need not be made collectively.
-   *
-   * This is a getter function that should complete in \f$ \Theta(1) \f$ time.
-   *
-   * This function when called on a valid channel instance will never fail.
-   */
-  __USED__ inline size_t getHeadPosition() const noexcept
-  {
-    return (_tail + _depth) % _capacity;
-  }
-
-  /**
-   * @returns The current position of the buffer head
-   *
-   * Returns the position of the buffer head to set as offset for sending/receiving operations.
-   *
-   * This is a one-sided blocking call that need not be made collectively.
-   *
-   * This is a getter function that should complete in \f$ \Theta(1) \f$ time.
-   *
-   * This function when called on a valid channel instance will never fail.
-   */
-  __USED__ inline size_t getTailPosition() const noexcept
-  {
-    return _tail;
   }
 
   /**
@@ -211,42 +126,6 @@ class Base
   MemorySlot *const _coordinationBuffer;
 
   /**
-   * This function increases the circular buffer depth (e.g., when an element is pushed) by advancing a virtual head.
-   * The head cannot advance in such a way that the depth exceeds capacity.
-   *
-   * \param[in] n The number of positions to advance
-   */
-  __USED__ inline void advanceHead(const size_t n = 1)
-  {
-    // Calculating new depth
-    const auto newDepth = _depth + n;
-
-    // Sanity check
-    if (newDepth > _capacity) HICR_THROW_FATAL("Channel's circular new buffer depth (_depth (%lu) + n (%lu) = %lu) exceeded capacity (%lu) on increase. This is probably a bug in HiCR.\n", _depth, n, newDepth, _capacity);
-
-    // Storing new depth
-    _depth = newDepth;
-  }
-
-  /**
-   * This function advances buffer tail (e.g., when an element is popped). It goes back around if the capacity is exceeded
-   * The tail cannot advanced more than the current depth (that would mean that more elements were consumed than pushed).
-   *
-   * \param[in] n The number of positions to advance the head of the circular buffer
-   */
-  __USED__ inline void advanceTail(const size_t n = 1)
-  {
-    // Sanity check
-    if (n > _depth) HICR_THROW_FATAL("Channel's circular buffer depth (%lu) smaller than number of elements to decrease on advance tail. This is probably a bug in HiCR.\n", _depth, n);
-
-    // Decrease depth
-    _depth -= n;
-
-    // Advance tail
-    _tail = (_tail + n) % _capacity;
-  }
-
-  /**
    * Local memory slot to update the tail position
    */
   MemorySlot *const _poppedTokensSlot;
@@ -265,11 +144,6 @@ class Base
    * Token size
    */
   const size_t _tokenSize;
-
-  /**
-   * How many tokens fit in the buffer
-   */
-  const size_t _capacity;
 
   /**
    * Buffer position at the head
