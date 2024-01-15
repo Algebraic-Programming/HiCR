6be5b89ccd359c83e9d443a8d8af25d47c68ae25
Sergio Martin <sergio.miguel.martin@huawei.com>
2024-01-15 14:16:33 +0100

    Progress with the machine model example

--- examples/machineModel/source/include/coordinator.hpp
+++ examples/machineModel/source/include/coordinator.hpp
@@ -15,17 +15,30 @@
 #include <backends/ascend/L0/device.hpp>
 #endif
 
+// This struct hold the information of a detected instance including its topology
+struct detectedInstance_t
+{
+  // Pointer to the detected instance
+  std::shared_ptr<HiCR::L0::Instance> instance;
+
+  // Detected topology of the given instance
+  HiCR::L0::Topology topology;
+};
+
 // This struct hold the information of an instance to create, as described by the machine model
 struct requestedInstance_t
 {
-  // Task name to execute
-  std::string taskName;
+  // Identifier of the task to execute
+  int taskId;
 
   // Number of replicas of this instance to create
   size_t replicaCount;
 
   // Requested topology for this instance, in HiCR format
   HiCR::L0::Topology topology;
+
+  // Pointer to the assigned instance
+  std::shared_ptr<HiCR::L0::Instance> instance;
 };
 
 void finalizeExecution(HiCR::L1::InstanceManager &instanceManager, const int returnCode = 0)
@@ -51,8 +64,8 @@ HiCR::L0::Topology parseTopology(const nlohmann::json& topologyJson)
   HiCR::L0::Topology topology;
 
   // Parsing CPU cores entry
-  if (topologyJson.contains("CPU Cores") == false) throw std::runtime_error("Error: the requested instance topoogy does not contain a 'CPU Cores' entry\n");
-  if (topologyJson["CPU Cores"].is_number_unsigned() == false) throw std::runtime_error("Error: The instance topology 'CPU Cores' entry is not a positive number\n");
+  if (topologyJson.contains("CPU Cores") == false) throw std::runtime_error("the requested instance topoogy does not contain a 'CPU Cores' entry\n");
+  if (topologyJson["CPU Cores"].is_number_unsigned() == false) throw std::runtime_error("The instance topology 'CPU Cores' entry is not a positive number\n");
   size_t numCores = topologyJson["CPU Cores"].get<size_t>();
 
   // Creating list of compute resources (CPU cores / processing units)
@@ -60,8 +73,8 @@ HiCR::L0::Topology parseTopology(const nlohmann::json& topologyJson)
   for (size_t i = 0; i < numCores; i++) computeResources.insert(std::make_shared<HiCR::backend::host::L0::ComputeResource>());
 
   // Parsing host RAM size entry
-  if (topologyJson.contains("Host RAM Size (Gb)") == false) throw std::runtime_error("Error: the requested instance topoogy does not contain a 'Host RAM Size (Gb)' entry\n");
-  if (topologyJson["Host RAM Size (Gb)"].is_number() == false) throw std::runtime_error("Error: The instance topology 'Host RAM Size (Gb)' entry is not a number\n");
+  if (topologyJson.contains("Host RAM Size (Gb)") == false) throw std::runtime_error("the requested instance topoogy does not contain a 'Host RAM Size (Gb)' entry\n");
+  if (topologyJson["Host RAM Size (Gb)"].is_number() == false) throw std::runtime_error("The instance topology 'Host RAM Size (Gb)' entry is not a number\n");
   auto hostRamSize = (size_t)std::ceil(topologyJson["Host RAM Size (Gb)"].get<double>() * 1024.0 * 1024.0 * 1024.0);
 
   // Creating list of memory spaces (only one, with the total host memory)
@@ -76,8 +89,8 @@ HiCR::L0::Topology parseTopology(const nlohmann::json& topologyJson)
   #ifdef _HICR_USE_ASCEND_BACKEND_
 
   // Parsing ascend device list
-  if (topologyJson.contains("Ascend Devices") == false) throw std::runtime_error("Error: the requested instance topoogy does not contain a 'Ascend Devices' entry\n");
-  if (topologyJson["Ascend Devices"].is_number_unsigned() == false) throw std::runtime_error("Error: The instance topology 'Ascend Devices' entry is not a positive number\n");
+  if (topologyJson.contains("Ascend Devices") == false) throw std::runtime_error("the requested instance topoogy does not contain a 'Ascend Devices' entry\n");
+  if (topologyJson["Ascend Devices"].is_number_unsigned() == false) throw std::runtime_error("The instance topology 'Ascend Devices' entry is not a positive number\n");
   size_t ascendDeviceCount = topologyJson["Ascend Devices"].get<size_t>();
 
   // Adding requested ascend devices, if any
@@ -89,35 +102,47 @@ HiCR::L0::Topology parseTopology(const nlohmann::json& topologyJson)
   return topology;
 }
 
-std::vector<std::shared_ptr<requestedInstance_t>> parseMachineModel(const nlohmann::json& machineModelJson)
+bool isTopologySubset(const HiCR::L0::Topology& a, const HiCR::L0::Topology& b)
+{
+  return false;
+}
+
+std::vector<requestedInstance_t> parseMachineModel(const nlohmann::json& machineModelJson)
 {
   // Storage for the parsed instances from the machine model
-  std::vector<std::shared_ptr<requestedInstance_t>> requestedInstances;  
+  std::vector<requestedInstance_t> requestedInstances;  
 
   // Checking for correct format in the machine model
-  if (machineModelJson.contains("Instances") == false) throw std::runtime_error("Error: the machine model does not contain an 'Instances' entry\n");
-  if (machineModelJson["Instances"].is_array() == false) throw std::runtime_error("Error: The 'Instances' entry in the machine model is not an array\n");
+  if (machineModelJson.contains("Instances") == false) throw std::runtime_error("the machine model does not contain an 'Instances' entry\n");
+  if (machineModelJson["Instances"].is_array() == false) throw std::runtime_error("The 'Instances' entry in the machine model is not an array\n");
 
   // Now iterating over all requested instances
   for (const auto& instance : machineModelJson["Instances"])
   {
     // Storage for the new requested instance to add
-    auto newRequestedInstance = std::make_shared<requestedInstance_t>();
+    requestedInstance_t newRequestedInstance;
 
     // Parsing task name
-    if (instance.contains("Task") == false) throw std::runtime_error("Error: the requested instance does not contain a 'Task' entry\n"); 
-    if (instance["Task"].is_string() == false) throw std::runtime_error("Error: The instance 'Task' entry is not a string\n");
-    newRequestedInstance->taskName = instance["Task"].get<std::string>();
+    if (instance.contains("Task") == false) throw std::runtime_error("the requested instance does not contain a 'Task' entry\n"); 
+    if (instance["Task"].is_string() == false) throw std::runtime_error("The instance 'Task' entry is not a string\n");
+    auto taskName = instance["Task"].get<std::string>();
+
+    // Matching task name to the corrresponding Id
+    bool recognizedTaskName = false;
+    if (taskName == "Task A") { newRequestedInstance.taskId = TASK_A_EXECUTION_UNIT_ID; recognizedTaskName = true; }
+    if (taskName == "Task B") { newRequestedInstance.taskId = TASK_B_EXECUTION_UNIT_ID; recognizedTaskName = true; }
+    if (taskName == "Task C") { newRequestedInstance.taskId = TASK_C_EXECUTION_UNIT_ID; recognizedTaskName = true; }
+    if (recognizedTaskName == false) throw std::runtime_error("The instance 'Task Name' is not recognized\n");
 
     // Parsing replica count
-    if (instance.contains("Replicas") == false) throw std::runtime_error("Error: the requested instance does not contain a 'Replicas' entry\n");
-    if (instance["Replicas"].is_number_unsigned() == false) throw std::runtime_error("Error: The instance 'Replicas' entry is not an unsigned number\n");
-    newRequestedInstance->replicaCount = instance["Replicas"].get<size_t>();
+    if (instance.contains("Replicas") == false) throw std::runtime_error("the requested instance does not contain a 'Replicas' entry\n");
+    if (instance["Replicas"].is_number_unsigned() == false) throw std::runtime_error("The instance 'Replicas' entry is not an unsigned number\n");
+    newRequestedInstance.replicaCount = instance["Replicas"].get<size_t>();
 
     // Parsing requested topology
-    if (instance.contains("Topology") == false) throw std::runtime_error("Error: the requested instance does not contain a 'Topology' entry\n");
-    if (instance["Topology"].is_object() == false) throw std::runtime_error("Error: The instance 'Topology' entry is not an object\n");
-    newRequestedInstance->topology = parseTopology(instance["Topology"]);
+    if (instance.contains("Topology") == false) throw std::runtime_error("the requested instance does not contain a 'Topology' entry\n");
+    if (instance["Topology"].is_object() == false) throw std::runtime_error("The instance 'Topology' entry is not an object\n");
+    newRequestedInstance.topology = parseTopology(instance["Topology"]);
 
     // Adding newly requested instance to the collection
     requestedInstances.push_back(newRequestedInstance);
@@ -127,36 +152,24 @@ std::vector<std::shared_ptr<requestedInstance_t>> parseMachineModel(const nlohma
   return requestedInstances;
 }
 
-
-void coordinatorFc(HiCR::L1::InstanceManager &instanceManager, const std::string& machineModelFilePath)
+std::vector<detectedInstance_t> detectInstances(HiCR::L1::InstanceManager &instanceManager)
 {
+  // Storage for the detected instances
+  std::vector<detectedInstance_t> detectedInstances;
+  
   // Getting the pointer to our own (coordinator) instance
   auto coordinator = instanceManager.getCurrentInstance();
 
-  // Reading from machine model file
-  std::string machineModelRaw;
-  auto status = loadStringFromFile(machineModelRaw, machineModelFilePath);
-  if (status == false) {  fprintf(stderr, "Error: could not read from machine model file: '%s'\n", machineModelFilePath.c_str()); finalizeExecution(instanceManager, -1); }
-
-  // Parsing received machine model file
-  nlohmann::json machineModelJson;
-  try { machineModelJson = nlohmann::json::parse(machineModelRaw); }
-   catch (const std::exception& e)
-    {  fprintf(stderr, "Error: could not parse JSON from machine model file: '%s'. Reason: '%s'\n", machineModelFilePath.c_str(), e.what()); finalizeExecution(instanceManager, -1); }
-
-  // Parsing the machine model into its requested instances. Here the vector implies ordering, which allows the user specify which instances need to be allocated first
-  std::vector<std::shared_ptr<requestedInstance_t>> requestedInstances;
-  try { requestedInstances = parseMachineModel(machineModelJson); }
-   catch (const std::exception& e)
-    {  fprintf(stderr, "Error while parsing the machine model. Reason: '%s'\n", e.what());  finalizeExecution(instanceManager, -1); }
-
-  // Obtaining the currently launched instances
-  auto &instances = instanceManager.getInstances();
-
   // Obtaining each of the instances HiCR topology
-  for (const auto &instance : instances)
+  for (const auto &instance : instanceManager.getInstances())
   if (instance->getId() != coordinator->getId())
   {
+    // Storage for the newly detected instance
+    detectedInstance_t detectedInstance;
+
+    // Assigning instance pointer
+    detectedInstance.instance = instance;
+
     // Running the RPC that obtains the instance's serialized topology
     instanceManager.execute(*instance, PROCESSING_UNIT_ID, TOPOLOGY_EXECUTION_UNIT_ID);
 
@@ -169,30 +182,98 @@ void coordinatorFc(HiCR::L1::InstanceManager &instanceManager, const std::string
     // Parsing serialized raw topology into a json object
     auto topologyJson = nlohmann::json::parse(serializedTopology);
 
-    // HiCR topology object to obtain
-    HiCR::L0::Topology topology;
+    // Obtaining the host topology from the serialized object
+    detectedInstance.topology.merge(HiCR::backend::host::hwloc::L1::TopologyManager::deserializeTopology(topologyJson));
 
-    // Obtaining the topology from the serialized object
-    topology.merge(HiCR::backend::host::hwloc::L1::TopologyManager::deserializeTopology(topologyJson));
-
+    // Obtaining the ascend topology (if enabled at compilation time)
     #ifdef _HICR_USE_ASCEND_BACKEND_
-    topology.merge(HiCR::backend::ascend::L1::TopologyManager::deserializeTopology(topologyJson));
+    detectedInstance.topology.merge(HiCR::backend::ascend::L1::TopologyManager::deserializeTopology(topologyJson));
     #endif // _HICR_USE_ASCEND_BACKEND_
+
+    // Adding detected instance to the storage
+    detectedInstances.push_back(detectedInstance);
   }
 
-  // Printing instance information and invoking a simple RPC if its not ourselves
-  for (const auto &instance : instances)
-    if (instance->getId() != coordinator->getId())
-      instanceManager.execute(*instance, PROCESSING_UNIT_ID, TASK_A_EXECUTION_UNIT_ID);
+  // Returning detected instances
+  return detectedInstances;
+}
 
-  // Getting return values from the RPCs containing each of the worker's topology
-  for (const auto &instance : instances) if (instance != coordinator)
+void executeRequests(HiCR::L1::InstanceManager &instanceManager, std::vector<requestedInstance_t>& requestedInstances)
+{
+  // Getting information about the currently launched instances and their topology
+  auto detectedInstances = detectInstances(instanceManager);
+
+  // Now matching requested instances to actual instances, creating new ones if the detected ones do not satisfy their topology requirements
+  for (size_t i = 0; i < requestedInstances.size(); i++)
+  {
+    // Flag to store whether the request has been assigned an instances
+    bool requestAssigned = false;
+
+    // Try to match the requested instances against one of the detected instances
+    for (auto dItr = detectedInstances.begin(); dItr != detectedInstances.end() && requestAssigned == false; dItr++)
+    {
+      // Check if the detected instance topology satisfied the request do the following
+      if (isTopologySubset(requestedInstances[i].topology, dItr->topology))
+      {
+        // Assign the instance to the request
+        requestedInstances[i].instance = dItr->instance;
+
+        // Mark request as assigned to continue break the loops
+        requestAssigned = true;
+
+        // Remove detected instance from the list so it's not assigned to another request
+        detectedInstances.erase(dItr);
+      }
+    }
+
+    // If no remaining detected instances satisfied the request, then try to create a new instance ad hoc
+    
+    // Adding new instance to the detected instance set
+
+    // If no instances could be created, then abort
+    std::string errorMsg = std::string("Could not assign nor create an instance for request ") + std::to_string(i);
+    if (requestAssigned == false) throw std::runtime_error(errorMsg.c_str()); 
+  }
+}
+
+void coordinatorFc(HiCR::L1::InstanceManager &instanceManager, const std::string& machineModelFilePath)
+{
+  // Getting the pointer to our own (coordinator) instance
+  auto coordinator = instanceManager.getCurrentInstance();
+
+  // Reading from machine model file
+  std::string machineModelRaw;
+  auto status = loadStringFromFile(machineModelRaw, machineModelFilePath);
+  if (status == false) {  fprintf(stderr, "could not read from machine model file: '%s'\n", machineModelFilePath.c_str()); finalizeExecution(instanceManager, -1); }
+
+  // Parsing received machine model file
+  nlohmann::json machineModelJson;
+  try { machineModelJson = nlohmann::json::parse(machineModelRaw); }
+   catch (const std::exception& e)
+    {  fprintf(stderr, "could not parse JSON from machine model file: '%s'. Reason: '%s'\n", machineModelFilePath.c_str(), e.what()); finalizeExecution(instanceManager, -1); }
+
+  // Parsing the machine model into its requested instances. Here the vector implies ordering, which allows the user specify which instances need to be allocated first
+  std::vector<requestedInstance_t> requestedInstances;
+  try { requestedInstances = parseMachineModel(machineModelJson); }
+   catch (const std::exception& e)
+    {  fprintf(stderr, "Error while parsing the machine model. Reason: '%s'\n", e.what());  finalizeExecution(instanceManager, -1); }
+
+  // Execute requests by finding or creating an instance that matches their topology requirements
+  try { executeRequests(instanceManager, requestedInstances); }
+   catch (const std::exception& e)
+    {  fprintf(stderr, "Error while exeucuting requests. Reason: '%s'\n", e.what());  finalizeExecution(instanceManager, -1); }
+
+  // Running the assigned task id in the correspondng instance
+  for (auto &r : requestedInstances) instanceManager.execute(*r.instance, PROCESSING_UNIT_ID, r.taskId);
+
+  // Now waiting for return values to arrive
+  for (auto &r : requestedInstances)
   {
     // Getting return value as a memory slot
-    auto returnValue = instanceManager.getReturnValue(*instance);
+    auto returnValue = instanceManager.getReturnValue(*r.instance);
 
     // Printing return value
-    printf("[Coordinator] Received from instance %lu: '%s'\n", instance->getId(), (const char*)returnValue->getPointer());
+    printf("[Coordinator] Received from instance %lu: '%s'\n", r.instance->getId(), (const char*)returnValue->getPointer());
   }
 
   // Finalizing execution for all instances
