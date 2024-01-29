1cb97ba2c168044d42fd767313957d5545816541
Sergio Martin <sergio.miguel.martin@huawei.com>
2024-01-29 08:49:04 +0100

    Small restructuring to the machine model to make it independent from JSON inpus

--- examples/machineModel/source/include/coordinator.hpp
+++ examples/machineModel/source/include/coordinator.hpp
@@ -24,6 +24,95 @@ void finalizeExecution(HiCR::L1::InstanceManager& instanceManager, const int ret
   exit(returnCode);
 }
 
+HiCR::L0::Topology parseTopology(const nlohmann::json &topologyJson)
+{
+  // Storage for the HiCR-formatted topology to create
+  HiCR::L0::Topology topology;
+
+  // Parsing CPU cores entry
+  if (topologyJson.contains("CPU Cores") == false) throw std::runtime_error("the requested instance topoogy does not contain a 'CPU Cores' entry\n");
+  if (topologyJson["CPU Cores"].is_number_unsigned() == false) throw std::runtime_error("The instance topology 'CPU Cores' entry is not a positive number\n");
+  size_t numCores = topologyJson["CPU Cores"].get<size_t>();
+
+  // Creating list of compute resources (CPU cores / processing units)
+  HiCR::L0::Device::computeResourceList_t computeResources;
+  for (size_t i = 0; i < numCores; i++) computeResources.insert(std::make_shared<HiCR::backend::host::L0::ComputeResource>());
+
+  // Parsing host RAM size entry
+  if (topologyJson.contains("Host RAM Size (Gb)") == false) throw std::runtime_error("the requested instance topoogy does not contain a 'Host RAM Size (Gb)' entry\n");
+  if (topologyJson["Host RAM Size (Gb)"].is_number() == false) throw std::runtime_error("The instance topology 'Host RAM Size (Gb)' entry is not a number\n");
+  auto hostRamSize = (size_t)std::ceil(topologyJson["Host RAM Size (Gb)"].get<double>() * 1024.0 * 1024.0 * 1024.0);
+
+  // Creating list of memory spaces (only one, with the total host memory)
+  HiCR::L0::Device::memorySpaceList_t memorySpaces({std::make_shared<HiCR::backend::host::L0::MemorySpace>(hostRamSize)});
+
+  // Creating the CPU (NUMA Domain) device type with the assigned number of cores
+  auto hostDevice = std::make_shared<HiCR::backend::host::L0::Device>(0, computeResources, memorySpaces);
+
+  // Adding host device to the topology
+  topology.addDevice(hostDevice);
+
+#ifdef _HICR_USE_ASCEND_BACKEND_
+
+  // Parsing ascend device list
+  if (topologyJson.contains("Ascend Devices") == false) throw std::runtime_error("the requested instance topoogy does not contain a 'Ascend Devices' entry\n");
+  if (topologyJson["Ascend Devices"].is_number_unsigned() == false) throw std::runtime_error("The instance topology 'Ascend Devices' entry is not a positive number\n");
+  size_t ascendDeviceCount = topologyJson["Ascend Devices"].get<size_t>();
+
+  // Adding requested ascend devices, if any
+  for (size_t i = 0; i < ascendDeviceCount; i++) topology.addDevice(std::make_shared<HiCR::backend::ascend::L0::Device>());
+
+#endif
+
+  // Returning the HiCR-formatted topology
+  return topology;
+}
+
+/**
+ * This function takes a valid JSON-based description of a machine model and parses it into HiCR request objects that can be
+ * satisfied later by creating a new instance using the instance manager
+ *
+ * @param[in] machineModelJson The machine model to parse, in JSON format
+ * @return A vector of requests, in the same order as provided in the JSON file
+ */
+std::vector<HiCR::MachineModel::request_t> parseMachineModel(const nlohmann::json &machineModelJson)
+{
+  // Storage for the parsed instances from the machine model
+  std::vector<HiCR::MachineModel::request_t> requests;
+
+  // Checking for correct format in the machine model
+  if (machineModelJson.contains("Instances") == false) throw std::runtime_error("the machine model does not contain an 'Instances' entry\n");
+  if (machineModelJson["Instances"].is_array() == false) throw std::runtime_error("The 'Instances' entry in the machine model is not an array\n");
+
+  // Now iterating over all requested instances
+  for (const auto &instance : machineModelJson["Instances"])
+  {
+    // Storage for the new requested instance to add
+    HiCR::MachineModel::request_t newRequestedInstance;
+
+    // Parsing task name
+    if (instance.contains("Task") == false) throw std::runtime_error("the requested instance does not contain a 'Task' entry\n");
+    if (instance["Task"].is_string() == false) throw std::runtime_error("The instance 'Task' entry is not a string\n");
+    newRequestedInstance.taskName = instance["Task"].get<std::string>();
+
+    // Parsing replica count
+    if (instance.contains("Replicas") == false) throw std::runtime_error("the requested instance does not contain a 'Replicas' entry\n");
+    if (instance["Replicas"].is_number_unsigned() == false) throw std::runtime_error("The instance 'Replicas' entry is not an unsigned number\n");
+    newRequestedInstance.replicaCount = instance["Replicas"].get<size_t>();
+
+    // Parsing requested topology
+    if (instance.contains("Topology") == false) throw std::runtime_error("the requested instance does not contain a 'Topology' entry\n");
+    if (instance["Topology"].is_object() == false) throw std::runtime_error("The instance 'Topology' entry is not an object\n");
+    newRequestedInstance.topology = parseTopology(instance["Topology"]);
+
+    // Adding newly requested instance to the collection
+    requests.push_back(newRequestedInstance);
+  }
+
+  // Returning parsed requested instances
+  return requests;
+}
+
 /*
  * This function allows us to customize the criteria that determine whether the detected topology satisfies one of our requests
  */
@@ -123,7 +212,7 @@ void coordinatorFc(HiCR::L1::InstanceManager& instanceManager, const std::string
   std::vector<HiCR::MachineModel::request_t> requests;
   try
   {
-    requests = machineModel.parse(machineModelJson);
+    requests = parseMachineModel(machineModelJson);
   }
   catch (const std::exception &e)
   {
