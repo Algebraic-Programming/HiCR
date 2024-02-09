d2cfecc7e6c7c614d1f2d4594a8cad9e341a527b
Sergio Martin <sergio.miguel.martin@huawei.com>
2024-02-09 15:46:39 +0100

    Making main slimmer

--- examples/runtime/source/include/machineModel.hpp
+++ examples/runtime/source/include/machineModel.hpp
@@ -172,3 +172,41 @@ bool isTopologyAcceptable(const HiCR::L0::Topology &a, const HiCR::L0::Topology 
   // If no criteria failed, return true
   return true;
 }
+
+std::vector<HiCR::MachineModel::request_t> loadMachineModelFromFile(const std::string& machineModelFile)
+{
+  // Reading from machine model file
+  std::string machineModelRaw;
+  auto status = loadStringFromFile(machineModelRaw, machineModelFile);
+  if (status == false)
+  {
+    fprintf(stderr, "could not read from machine model file: '%s'\n", machineModelFile.c_str());
+    HiCR::Runtime::abort(-1);
+  }
+
+  // Parsing received machine model file
+  nlohmann::json machineModelJson;
+  try
+  {
+    machineModelJson = nlohmann::json::parse(machineModelRaw);
+  }
+  catch (const std::exception &e)
+  {
+    fprintf(stderr, "could not parse JSON from machine model file: '%s'. Reason: '%s'\n", machineModelFile.c_str(), e.what());
+    HiCR::Runtime::abort(-1);
+  }
+
+  // Parsing the machine model into a request vector. Here the vector implies ordering, which allows the user specify which instances need to be allocated first
+  std::vector<HiCR::MachineModel::request_t> requests;
+  try
+  {
+    requests = parseMachineModel(machineModelJson);
+  }
+  catch (const std::exception &e)
+  {
+    fprintf(stderr, "Error while parsing the machine model. Reason: '%s'\n", e.what());
+    HiCR::Runtime::abort(-1);
+  }
+
+  return requests;
+}
--- examples/runtime/source/main.cpp
+++ examples/runtime/source/main.cpp
@@ -18,48 +18,17 @@ int main(int argc, char *argv[])
   if (argc != 2)
   {
     fprintf(stderr, "Launch error. No machine model file provided\n");
-    HiCR::Runtime::finalize();
-    return -1;
+    HiCR::Runtime::abort(-1);
   }
 
   // Parsing number of instances requested
   auto machineModelFile = std::string(argv[1]);
 
-  // Reading from machine model file
-  std::string machineModelRaw;
-  auto status = loadStringFromFile(machineModelRaw, machineModelFile);
-  if (status == false)
-  {
-    fprintf(stderr, "could not read from machine model file: '%s'\n", machineModelFile.c_str());
-    HiCR::Runtime::abort(-1);
-  }
-
-  // Parsing received machine model file
-  nlohmann::json machineModelJson;
-  try
-  {
-    machineModelJson = nlohmann::json::parse(machineModelRaw);
-  }
-  catch (const std::exception &e)
-  {
-    fprintf(stderr, "could not parse JSON from machine model file: '%s'. Reason: '%s'\n", machineModelFile.c_str(), e.what());
-    HiCR::Runtime::abort(-1);
-  }
-
-  // Parsing the machine model into a request vector. Here the vector implies ordering, which allows the user specify which instances need to be allocated first
-  std::vector<HiCR::MachineModel::request_t> requests;
-  try
-  {
-    requests = parseMachineModel(machineModelJson);
-  }
-  catch (const std::exception &e)
-  {
-    fprintf(stderr, "Error while parsing the machine model. Reason: '%s'\n", e.what());
-    HiCR::Runtime::abort(-1);
-  }
+  // Loading machine model
+  auto machineModel = loadMachineModelFromFile(machineModelFile);
 
   // Finally, deploying machine model
-  HiCR::Runtime::deploy(requests, &isTopologyAcceptable);
+  HiCR::Runtime::deploy(machineModel, &isTopologyAcceptable);
   
   // Finalizing runtime
   HiCR::Runtime::finalize();
