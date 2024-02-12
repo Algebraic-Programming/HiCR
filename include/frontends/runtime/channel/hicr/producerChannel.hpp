833074cc5918eb1a722dc25c6d359080dd194e67
Sergio Martin <sergio.miguel.martin@huawei.com>
2024-02-12 09:46:11 +0100

    Bifurcating implementation of runtime channels to enable interoperability with Yuan Rong

---
+++ include/frontends/runtime/channel/hicr/producerChannel.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <frontends/channel/variableSize/spsc/producer.hpp>
+#include "common.hpp"
+
+namespace HiCR
+{
+namespace runtime
+{
+
+ typedef HiCR::channel::variableSize::SPSC::Producer ProducerChannel;
+
+} // namespace HiCR
+
+} // namespace HiCR
--- include/frontends/runtime/coordinator.hpp
+++ include/frontends/runtime/coordinator.hpp
@@ -15,8 +15,14 @@
 #include <memory>
 #include <hicr/definitions.hpp>
 #include <hicr/exceptions.hpp>
-#include "channel.hpp"
 #include "instance.hpp"
+
+// For interoperability with YuanRong, we bifurcate implementations using different includes
+#ifdef _HICR_USE_YUANRONG_BACKEND_
+  #include <channel/yuanrong/producerChannel.hpp>
+#else
+  #include "channel/hicr/producerChannel.hpp"
+#endif
 
 namespace HiCR
 {
@@ -49,9 +55,9 @@ class Coordinator final : public runtime::Instance
     const std::shared_ptr<HiCR::L0::Instance> hicrInstance;
 
     /**
-     * Consumer channel to receive RPCs from the coordinator instance
+     * Producer channels to send RPCs to the worker instances
      */ 
-    std::shared_ptr<runtime::Channel> rpcChannel;
+    std::shared_ptr<runtime::ProducerChannel> rpcChannel;
   };
 
   Coordinator() = delete;
@@ -101,6 +107,29 @@ class Coordinator final : public runtime::Instance
 
     // Initializing RPC channels
     initializeRPCChannels();
+    
+    // Accessing first topology manager detected
+    auto& tm = *_topologyManagers[0];
+
+    // Gathering topology from the topology manager
+    const auto t = tm.queryTopology();
+
+    // Selecting first device
+    auto d = *t.getDevices().begin();
+
+    // Getting memory space list from device
+    auto memSpaces = d->getMemorySpaceList();
+
+    // Grabbing first memory space for buffering
+    auto bufferMemorySpace = *memSpaces.begin();
+
+    // Sending initial message to all workers
+    for (auto& worker : _workers)
+    {
+      std::string message = "Hello from the coordinator";
+      auto messageSendSlot = _memoryManager->registerLocalMemorySlot(bufferMemorySpace, message.data(), message.size());
+      worker.rpcChannel->push(messageSendSlot);
+    }
 
     // Launching worker's entry point function     
     for (auto &w : _workers) _instanceManager->launchRPC(*w.hicrInstance, w.entryPoint); 
@@ -119,117 +148,21 @@ class Coordinator final : public runtime::Instance
 
   private:
 
-  void initializeRPCChannels()
-  {
-    ////////// Creating producer channels to send variable sized RPCs fto the workers
-
-      // Accessing first topology manager detected
-      auto& tm = *_topologyManagers[0];
-
-      // Gathering topology from the topology manager
-      const auto t = tm.queryTopology();
-
-      // Selecting first device
-      auto d = *t.getDevices().begin();
-
-      // Getting memory space list from device
-      auto memSpaces = d->getMemorySpaceList();
-
-      // Grabbing first memory space for buffering
-      auto bufferMemorySpace = *memSpaces.begin();
-
-      // Getting required buffer size for coordination buffers
-      auto coordinationBufferSize = HiCR::channel::variableSize::Base::getCoordinationBufferSize();
-
-      // Storage for dedicated channels
-      std::vector<std::pair<HiCR::L0::Instance::instanceId_t, std::shared_ptr<HiCR::L0::LocalMemorySlot>>> coordinationBufferMessageSizesVector;
-      std::vector<std::pair<HiCR::L0::Instance::instanceId_t, std::shared_ptr<HiCR::L0::LocalMemorySlot>>> coordinationBufferMessagePayloadsVector;
-      std::vector<std::shared_ptr<HiCR::L0::LocalMemorySlot>> sizeInfoBufferMemorySlotVector;
-      
-      // Create coordinator buffers for each of the channels (one per worker)
-      for (size_t i = 0; i < _workers.size(); i++)
-      {
-        // Allocating coordination buffers
-        auto coordinationBufferMessageSizes    = _memoryManager->allocateLocalMemorySlot(bufferMemorySpace, coordinationBufferSize);
-        auto coordinationBufferMessagePayloads = _memoryManager->allocateLocalMemorySlot(bufferMemorySpace, coordinationBufferSize);
-        auto sizeInfoBufferMemorySlot          = _memoryManager->allocateLocalMemorySlot(bufferMemorySpace, sizeof(size_t));
-
-        // Initializing coordination buffers
-        HiCR::channel::variableSize::Base::initializeCoordinationBuffer(coordinationBufferMessageSizes);
-        HiCR::channel::variableSize::Base::initializeCoordinationBuffer(coordinationBufferMessagePayloads);
-
-        // Getting worker instance id
-        const auto workerInstanceId = _workers[i].hicrInstance->getId();
-
-        // Adding buffers to their respective vectors
-        coordinationBufferMessageSizesVector.push_back({workerInstanceId, coordinationBufferMessageSizes});
-        coordinationBufferMessagePayloadsVector.push_back({workerInstanceId, coordinationBufferMessagePayloads});
-        sizeInfoBufferMemorySlotVector.push_back(sizeInfoBufferMemorySlot);
-      }
-
-      // Exchanging local memory slots to become global for them to be used by the remote end
-      _communicationManager->exchangeGlobalMemorySlots(_HICR_RUNTIME_CHANNEL_WORKER_SIZES_BUFFER_TAG,                      {});
-      _communicationManager->fence(_HICR_RUNTIME_CHANNEL_WORKER_SIZES_BUFFER_TAG);
-
-      _communicationManager->exchangeGlobalMemorySlots(_HICR_RUNTIME_CHANNEL_WORKER_PAYLOAD_BUFFER_TAG,                    {});
-      _communicationManager->fence(_HICR_RUNTIME_CHANNEL_WORKER_PAYLOAD_BUFFER_TAG);
-
-      _communicationManager->exchangeGlobalMemorySlots(_HICR_RUNTIME_CHANNEL_WORKER_COORDINATION_BUFFER_SIZES_TAG,         {});
-      _communicationManager->fence(_HICR_RUNTIME_CHANNEL_WORKER_COORDINATION_BUFFER_SIZES_TAG);
-
-      _communicationManager->exchangeGlobalMemorySlots(_HICR_RUNTIME_CHANNEL_WORKER_COORDINATION_BUFFER_PAYLOADS_TAG,      {});
-      _communicationManager->fence(_HICR_RUNTIME_CHANNEL_WORKER_COORDINATION_BUFFER_PAYLOADS_TAG);
-
-      _communicationManager->exchangeGlobalMemorySlots(_HICR_RUNTIME_CHANNEL_COORDINATOR_COORDINATION_BUFFER_SIZES_TAG,    coordinationBufferMessageSizesVector);
-      _communicationManager->fence(_HICR_RUNTIME_CHANNEL_COORDINATOR_COORDINATION_BUFFER_SIZES_TAG);
-      
-      _communicationManager->exchangeGlobalMemorySlots(_HICR_RUNTIME_CHANNEL_COORDINATOR_COORDINATION_BUFFER_PAYLOADS_TAG, coordinationBufferMessagePayloadsVector);
-      _communicationManager->fence(_HICR_RUNTIME_CHANNEL_COORDINATOR_COORDINATION_BUFFER_PAYLOADS_TAG); 
-
-      printf("Memory slots exchanged\n");
-
-      // Creating producer channels
-      for (size_t i = 0; i < _workers.size(); i++)
-      {
-        // Getting worker
-        const auto& worker = _workers[i];
-
-        // Getting worker instance id
-        const auto workerInstanceId = worker.hicrInstance->getId();
-
-        // Obtaining the globally exchanged memory slots
-        auto workerMessageSizesBuffer            = _communicationManager->getGlobalMemorySlot(_HICR_RUNTIME_CHANNEL_WORKER_SIZES_BUFFER_TAG,                      workerInstanceId);
-        auto workerMessagePayloadBuffer          = _communicationManager->getGlobalMemorySlot(_HICR_RUNTIME_CHANNEL_WORKER_PAYLOAD_BUFFER_TAG,                    workerInstanceId);
-        auto coordinatorSizesCoordinatorBuffer   = _communicationManager->getGlobalMemorySlot(_HICR_RUNTIME_CHANNEL_COORDINATOR_COORDINATION_BUFFER_SIZES_TAG,    workerInstanceId);
-        auto coordinatorPayloadCoordinatorBuffer = _communicationManager->getGlobalMemorySlot(_HICR_RUNTIME_CHANNEL_COORDINATOR_COORDINATION_BUFFER_PAYLOADS_TAG, workerInstanceId);
-
-        // Creating channel
-        auto producerChannel = HiCR::channel::variableSize::SPSC::Producer(
-          *_communicationManager,
-          sizeInfoBufferMemorySlotVector[i],
-          workerMessagePayloadBuffer,
-          workerMessageSizesBuffer,
-          coordinationBufferMessageSizesVector[i].second,
-          coordinationBufferMessagePayloadsVector[i].second,
-          coordinatorSizesCoordinatorBuffer,
-          coordinatorPayloadCoordinatorBuffer,
-          _HICR_RUNTIME_CHANNEL_PAYLOAD_CAPACITY,
-          sizeof(uint8_t),
-          _HICR_RUNTIME_CHANNEL_COUNT_CAPACITY
-        );
-
-        // Sending initial message
-        std::string message = "Hello from the coordinator";
-        auto messageSendSlot = _memoryManager->registerLocalMemorySlot(bufferMemorySpace, message.data(), message.size());
-        producerChannel.push(messageSendSlot);
-      }
-  }
+  // For interoperability with YuanRong, this function is implemented differently depended on the backend used
+  __USED__ inline void initializeRPCChannels();
 
  /**
   * Storage for the deployed workers. This object is only mantained and usable by the coordinator
   */
   std::vector<worker_t> _workers;
 };
+
+// For interoperability with YuanRong, we bifurcate implementations using different includes
+#ifdef _HICR_USE_YUANRONG_BACKEND_
+  #include <channel/yuanrong/producerChannelImpl.hpp>
+#else
+  #include "channel/hicr/producerChannelImpl.hpp"
+#endif
 
 } // namespace runtime
 
