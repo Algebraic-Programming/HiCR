aa0db1299c7419bde1308222aceb7856c3a51d21
Sergio Martin <sergio.miguel.martin@huawei.com>
2024-03-20 15:15:43 +0100

    Updating style

--- include/frontends/runtime/instance.hpp
+++ include/frontends/runtime/instance.hpp
@@ -59,11 +59,11 @@ class Instance
    * @param[in] topologyManagers The topology managers backend to use to discover the system's resources
    * @param[in] machineModel The machine model to use to deploy the workers
    */
-  Instance(HiCR::L1::InstanceManager*                instanceManager,
-           HiCR::L1::CommunicationManager*           communicationManager,
-           HiCR::L1::MemoryManager*                  memoryManager,
-           std::vector<HiCR::L1::TopologyManager *>  &topologyManagers,
-           HiCR::MachineModel*                       machineModel)
+  Instance(HiCR::L1::InstanceManager                *instanceManager,
+           HiCR::L1::CommunicationManager           *communicationManager,
+           HiCR::L1::MemoryManager                  *memoryManager,
+           std::vector<HiCR::L1::TopologyManager *> &topologyManagers,
+           HiCR::MachineModel                       *machineModel)
     : _HiCRInstance(instanceManager->getCurrentInstance()),
       _instanceManager(instanceManager),
       _communicationManager(communicationManager),
