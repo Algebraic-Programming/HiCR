acc28e6517fa92f8bed5f8a70a247e40a5557096
Sergio Martin <sergio.miguel.martin@huawei.com>
2024-03-27 07:56:54 +0100

    Renaming notify to  sync event

--- examples/tasking/runtime.hpp
+++ examples/tasking/runtime.hpp
@@ -249,7 +249,7 @@ class Runtime
    *
    * \param[in] task Suspended task pointer
    */
-  __USED__ inline void onTaskNotify(HiCR::tasking::Task *task)
+  __USED__ inline void onTaskSync(HiCR::tasking::Task *task)
   {
     // Re-adding task to the waiting list
     _waitingTaskQueue.push(task);
@@ -324,8 +324,8 @@ class Runtime
     _eventMap   = new HiCR::tasking::Task::taskEventMap_t();
 
     // Creating event map ands events
-    _eventMap->setEvent(HiCR::tasking::Task::event_t::onTaskFinish, [this](HiCR::tasking::Task *task) { onTaskFinish(task); }); //
-    _eventMap->setEvent(HiCR::tasking::Task::event_t::onTaskNotify, [this](HiCR::tasking::Task *task) { onTaskNotify(task); }); //
+    _eventMap->setEvent(HiCR::tasking::Task::event_t::onTaskFinish, [this](HiCR::tasking::Task *task) { onTaskFinish(task); });
+    _eventMap->setEvent(HiCR::tasking::Task::event_t::onTaskSync,   [this](HiCR::tasking::Task *task) { onTaskSync(task); });
 
     // Creating one worker per processung unit in the list
     for (auto &pu : _processingUnits)
