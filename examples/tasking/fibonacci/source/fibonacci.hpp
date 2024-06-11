7343f2196423dea45169d58b4ac13f076b3fb88a
Sergio Miguel Martin <sergio.miguel.martin@huawei.com>
2024-06-11 12:05:48 +0000

    Example/fibonacci

--- examples/tasking/fibonacci/source/fibonacci.hpp
+++ examples/tasking/fibonacci/source/fibonacci.hpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <chrono>
 #include <hicr/core/L0/device.hpp>
 #include <hicr/backends/host/L1/computeManager.hpp>
 #include "../runtime.hpp"
@@ -9,6 +10,10 @@ static std::atomic<uint64_t>                    _taskCounter;
 
 // This function serves to encode a fibonacci task label
 inline const uint64_t getFibonacciLabel(const uint64_t x, const uint64_t _initialValue) { return _initialValue; }
+
+// Lookup table for the number of tasks required for every fibonacci number
+uint64_t fibonacciTaskCount[] = {1,    1,    3,    5,     9,     15,    25,    41,    67,     109,    177,    287,    465,     753,     1219,   1973,
+                                 3193, 5167, 8361, 13529, 21891, 35421, 57313, 92735, 150049, 242785, 392835, 635621, 1028457, 1664079, 2692537};
 
 // Fibonacci without memoization to stress the tasking runtime
 uint64_t fibonacci(const uint64_t x)
@@ -40,8 +45,8 @@ uint64_t fibonacci(const uint64_t x)
 
 uint64_t fibonacciDriver(const uint64_t initialValue, HiCR::backend::host::L1::ComputeManager *computeManager, const HiCR::L0::Device::computeResourceList_t &computeResources)
 {
-  // Initializing taskr
-  taskr::Runtime taskr;
+  // Initializing taskr with the appropriate amount of max tasks
+  taskr::Runtime taskr(fibonacciTaskCount[initialValue]);
 
   // Setting event handler to re-add task to the queue after it suspended itself
   taskr.setEventHandler(HiCR::tasking::Task::event_t::onTaskSuspend, [&](HiCR::tasking::Task *task) { taskr.awakenTask(task); });
@@ -65,7 +70,12 @@ uint64_t fibonacciDriver(const uint64_t initialValue, HiCR::backend::host::L1::C
   taskr.addTask(initialTask);
 
   // Running taskr
+  auto startTime = std::chrono::high_resolution_clock::now();
   taskr.run(computeManager);
+  auto endTime     = std::chrono::high_resolution_clock::now();
+  auto computeTime = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - startTime);
+  printf("Running Time: %0.5fs\n", computeTime.count());
+  printf("Total Tasks: %lu\n", _taskCounter.load());
 
   // Returning fibonacci value
   return result;
