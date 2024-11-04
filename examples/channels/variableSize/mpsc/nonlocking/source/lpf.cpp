b606a1ae8d57bf7109a94ba3fb3809edced7ab10
Orestis Korakitis <orestis.korakitis@huawei.com>
2024-11-04 17:56:52 +0100

    examples: Raised size limits for memory and message slots in variable-size non-locking MPSC LPF example

--- examples/channels/variableSize/mpsc/nonlocking/source/lpf.cpp
+++ examples/channels/variableSize/mpsc/nonlocking/source/lpf.cpp
@@ -15,14 +15,14 @@ const int LPF_MPI_AUTO_INITIALIZE = 0;
  * in lpf_resize_memory_register . This value is currently
  * guessed as sufficiently large for a program
  */
-#define DEFAULT_MEMSLOTS 100
+#define DEFAULT_MEMSLOTS 128
 
 /**
  * #DEFAULT_MSGSLOTS The message slots used by LPF
  * in lpf_resize_message_queue . This value is currently
  * guessed as sufficiently large for a program
  */
-#define DEFAULT_MSGSLOTS 100
+#define DEFAULT_MSGSLOTS 128
 
 void spmd(lpf_t lpf, lpf_pid_t pid, lpf_pid_t nprocs, lpf_args_t args)
 {
