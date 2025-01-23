6f35572c148e9f35eb9600f602471c7daaadfd98
Kiril Dichev <kiril.dichev@huawei.com>
2025-01-23 22:11:36 +0100

    Changes for locking fixed-size MPSC: Setting a cached depth is probably not required in the case of locked access to consumer, as the locking should guarantee a degree of sequentiality between producer and consumer. Also, keep advancing head index inside pushing loop, as it is used in each iteration. Also stop using this silly local suffix for set_lpf.sh script

--- include/hicr/backends/lpf/set_lpf.sh
+++ include/hicr/backends/lpf/set_lpf.sh
@@ -1,4 +1,4 @@
-export PATH=$PWD/extern/lpf/build/local/bin:$PATH
-export LD_LIBRARY_PATH=$PWD/extern/lpf/build/local/lib:$LD_LIBRARY_PATH
-export LIBRARY_PATH=$PWD/extern/lpf/build/local/lib:$LIBRARY_PATH
-export CPATH=$PWD/extern/lpf/build/local/include:$CPATH
+export PATH=$PWD/extern/lpf/build/bin:$PATH
+export LD_LIBRARY_PATH=$PWD/extern/lpf/build/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$PWD/extern/lpf/build/lib:$LIBRARY_PATH
+export CPATH=$PWD/extern/lpf/build/include:$CPATH
