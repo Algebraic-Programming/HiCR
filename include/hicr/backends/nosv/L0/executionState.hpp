a6c2816a4ecb45341d08604a50e37f7466d3af5b
Sergio Martin <sergio.miguel.martin@huawei.com>
2025-03-05 10:12:11 +0100

    Adding pointer to execution state class

--- include/hicr/backends/nosv/L0/executionState.hpp
+++ include/hicr/backends/nosv/L0/executionState.hpp
@@ -21,43 +21,6 @@
 #include <hicr/backends/nosv/L0/executionUnit.hpp>
 #include <hicr/backends/nosv/common.hpp>
 
-/**
- * nosv MetaData place holder for the execution state
- */
-typedef struct _executionStateData
-{
-  /**
-   * Boolean to check if this is the worker mainLoop task
-   */
-  volatile bool mainLoop = false;
-
-  /**
-   * nosv barrier for the worker mainLoop task
-   * This is need as the submitted task from the worker mainLoop has to wait until the run_callback successfully executed
-   */
-  nosv_barrier_t mainLoop_barrier;
-
-  /**
-   * boolean to determine if the fc has finished.
-   */
-  volatile bool completed = false;
-
-  /**
-   * the passed function
-   */
-  std::function<void(void *)> fc = nullptr;
-
-  /**
-   * the function arguments
-   */
-  void *arg = nullptr;
-
-  /**
-   * The parent task from which this task has been submitted
-   */
-  nosv_task_t parent_task = NULL;
-} executionStateMetaData_t;
-
 namespace HiCR::backend::nosv::L0
 {
 
@@ -68,6 +31,44 @@ namespace HiCR::backend::nosv::L0
 class ExecutionState final : public HiCR::L0::ExecutionState
 {
   public:
+
+  /**
+   * nosv MetaData place holder for the execution state
+   */
+  struct taskMetadata_t
+  {
+    /**
+     * Boolean to check if this is the worker mainLoop task
+     */
+    volatile bool mainLoop = false;
+
+    /**
+     * nosv barrier for the worker mainLoop task
+     * This is need as the submitted task from the worker mainLoop has to wait until the run_callback successfully executed
+     */
+    nosv_barrier_t mainLoop_barrier;
+
+    /**
+     * the passed function
+     */
+    std::function<void(void *)> fc = nullptr;
+
+    /**
+     * the function arguments
+     */
+    void *arg = nullptr;
+
+    /**
+     * The parent task from which this task has been submitted
+     */
+    nosv_task_t parent_task = NULL;
+
+    /**
+     * A pointer to the execution state class corresponding to this task
+     */
+    ExecutionState* executionState = nullptr;
+  };
+
 
   /**
    * nosv task for the executionState
@@ -92,7 +93,7 @@ class ExecutionState final : public HiCR::L0::ExecutionState
     // nOS-V runtime callback wrapper for the fc
     nosv_task_run_callback_t run_callback = [](nosv_task_t task) {
       // Accessing metadata from the task
-      auto TaskMetadata = (executionStateMetaData_t *)getTaskMetadata(task);
+      auto TaskMetadata = (taskMetadata_t *)getTaskMetadata(task);
 
       // Unblocking the worker mainLoop  as the run_callback successfully has been called from here
       if (TaskMetadata->mainLoop) check(nosv_barrier_wait(TaskMetadata->mainLoop_barrier));
@@ -108,7 +109,7 @@ class ExecutionState final : public HiCR::L0::ExecutionState
       else { HICR_THROW_RUNTIME("Error: No valid callback function.\n"); }
 
       // Mark task as complete
-      TaskMetadata->completed = true;
+      TaskMetadata->executionState->_completed = true;
 
       // Resume the parent task as its child task has just finished
       if (!(TaskMetadata->mainLoop))
@@ -122,12 +123,13 @@ class ExecutionState final : public HiCR::L0::ExecutionState
     check(nosv_type_init(&_executionUnitTaskType, run_callback, NULL, NULL, "executionUnitTaskType", NULL, NULL, NOSV_TYPE_INIT_NONE));
 
     // nosv create the execution task
-    check(nosv_create(&_executionStateTask, _executionUnitTaskType, sizeof(executionStateMetaData_t), NOSV_CREATE_NONE));
+    check(nosv_create(&_executionStateTask, _executionUnitTaskType, sizeof(taskMetadata_t), NOSV_CREATE_NONE));
 
     // Access the execution state task metadata
-    auto metadata = (executionStateMetaData_t *)getTaskMetadata(_executionStateTask);
+    auto metadata = (taskMetadata_t *)getTaskMetadata(_executionStateTask);
 
     // Store the function and function argument in the metadata
+    metadata->executionState = this;
     metadata->fc  = c->getFunction();
     metadata->arg = argument;
   }
@@ -143,7 +145,7 @@ class ExecutionState final : public HiCR::L0::ExecutionState
     nosv_task_t current_task = nosv_self();
 
     // Access this execution state task metadata
-    auto metadata = (executionStateMetaData_t *)getTaskMetadata(_executionStateTask);
+    auto metadata = (taskMetadata_t *)getTaskMetadata(_executionStateTask);
 
     // Store the parent task in this task
     metadata->parent_task = current_task;
@@ -170,7 +172,7 @@ class ExecutionState final : public HiCR::L0::ExecutionState
     // Check if the self task is actually this execution state task (otherwise is illegal as only the self task is to suspend itself)
     if (self_task != _executionStateTask) HICR_THROW_RUNTIME("Those should be the same task. nosv_self(): %p, _executionStateTask: %p", &self_task, &_executionStateTask);
 
-    auto metadata = (executionStateMetaData_t *)getTaskMetadata(self_task);
+    auto metadata = (taskMetadata_t *)getTaskMetadata(self_task);
 
     // Resume the parent task to continue running other tasks.
     check(nosv_submit(metadata->parent_task, NOSV_SUBMIT_NONE));
@@ -186,9 +188,7 @@ class ExecutionState final : public HiCR::L0::ExecutionState
    */
   bool checkFinalizationImpl() override
   {
-    auto metadata = (executionStateMetaData_t *)getTaskMetadata(_executionStateTask);
-
-    return metadata->completed;
+    return _completed;
   }
 
   private:
@@ -197,6 +197,11 @@ class ExecutionState final : public HiCR::L0::ExecutionState
    * nosv task type of the executionUnit
    */
   nosv_task_type_t _executionUnitTaskType;
+
+  /**
+   * boolean to determine if the fc has finished.
+   */
+  volatile bool _completed = false;
 };
 
 } // namespace HiCR::backend::nosv::L0
