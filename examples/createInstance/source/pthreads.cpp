e0c502e7ab78a4a7586e1a9a7f2f152c892506fb
Luca Terracciano <luca.terraciano@huawei.com>
2025-10-10 10:07:18 +0200

    feat: add detection mechanism for pthreads

--- examples/createInstance/source/pthreads.cpp
+++ examples/createInstance/source/pthreads.cpp
@@ -1,51 +1,70 @@
 #include <cstdio>
 #include <pthread.h>
+#include <thread>
 
 #include <hicr/backends/hwloc/topologyManager.hpp>
 #include <hicr/backends/pthreads/instanceManager.hpp>
+#include <hicr/backends/pthreads/instancePool.hpp>
 
 #include "../include/createInstance.hpp"
 
 int main(int argc, char const *argv[])
 {
   // Check argvs
-  if (argc != 2) { HICR_THROW_RUNTIME("Pass the instance count as argument"); }
+  if (argc != 3) { HICR_THROW_RUNTIME("Pass the instance count as argument"); }
 
   // Get instance count
-  auto instanceCount = std::atoi(argv[1]);
+  size_t instanceCount    = std::atoi(argv[1]);
+  size_t instanceToCreate = std::atoi(argv[2]);
 
   // Determine the root instance id
   HiCR::Instance::instanceId_t rootInstanceId = pthread_self();
 
-  // Create barrier
-  pthread_barrier_t barrier{};
-  pthread_barrier_init(&barrier, nullptr, instanceCount);
+  // Create Instance pool
+  HiCR::backend::pthreads::InstancePool instancePool(0);
 
   // Declare entrypoint
   auto entrypoint = [&](HiCR::backend::pthreads::InstanceManager *creatorIm) {
-    auto im = HiCR::backend::pthreads::InstanceManager(rootInstanceId, creatorIm->getEntrypoint());
+    auto im = HiCR::backend::pthreads::InstanceManager(rootInstanceId, creatorIm->getEntrypoint(), instancePool);
     printf("[Instance %lu] Hello World\n", im.getCurrentInstance()->getId());
-    pthread_barrier_wait(&barrier);
-    printf("[Instance %lu] fininshing execution\n", im.getCurrentInstance()->getId());
   };
 
-  // Create instance manager
-  auto im = HiCR::backend::pthreads::InstanceManager(rootInstanceId, entrypoint);
+  // Define initial threads function
+  auto workload = [&]() {
+    // Create instance manager
+    auto im = HiCR::backend::pthreads::InstanceManager(rootInstanceId, entrypoint, instancePool);
 
-  // Discover local topology
-  auto tm = HiCR::backend::hwloc::TopologyManager::createDefault();
-  auto t  = tm->queryTopology();
+    // Detect already started instances
+    im.detectInstances(instanceCount);
 
-  // Create the new instance
-  createInstances(im, instanceCount, t);
+    // Discover local topology
+    auto tm = HiCR::backend::hwloc::TopologyManager::createDefault();
+    auto t  = tm->queryTopology();
 
-  // Wait barrier
-  pthread_barrier_wait(&barrier);
+    // Create the new instance
+    createInstances(im, instanceToCreate, t);
+
+    // Finalize instance manager
+    im.finalize();
+  };
+
+  std::vector<std::unique_ptr<std::thread>> initialThreads;
+  for (size_t i = 0; i < instanceCount - 1; i++)
+  {
+    // Create thread running the workload
+    auto thread = std::make_unique<std::thread>(workload);
+
+    // Add to the vector of initial threads
+    initialThreads.push_back(std::move(thread));
+  }
+
+  // Run the workload
+  workload();
+
+  // Wait for all the threads to join
+  for (auto &thread : initialThreads) { thread->join(); }
 
   printf("Terminating execution\n");
-
-  // Finalize instance manager
-  im.finalize();
 
   return 0;
 }
--- include/hicr/backends/pthreads/instanceManager.hpp
+++ include/hicr/backends/pthreads/instanceManager.hpp
@@ -5,6 +5,7 @@
 #include <hicr/core/instanceManager.hpp>
 
 #include "instance.hpp"
+#include "instancePool.hpp"
 
 namespace HiCR::backend::pthreads
 {
@@ -16,36 +17,99 @@ class InstanceManager;
   */
 typedef std::function<void(InstanceManager *)> entryPoint_t;
 
-class InstanceManager : public HiCR::InstanceManager
+class InstanceManager final : public HiCR::InstanceManager
 {
   public:
 
-  InstanceManager(Instance::instanceId_t rootInstanceId, entryPoint_t entrypoint)
+  InstanceManager(Instance::instanceId_t rootInstanceId, entryPoint_t entrypoint, InstancePool &instancePool)
     : HiCR::InstanceManager(),
       _rootInstanceId(rootInstanceId),
-      _entrypoint(entrypoint)
+      _entrypoint(entrypoint),
+      _instancePool(instancePool)
   {
-    setCurrentInstance(std::make_shared<Instance>(pthread_self(), rootInstanceId));
+    auto currentInstance = std::make_shared<Instance>(pthread_self(), _rootInstanceId);
+    setCurrentInstance(currentInstance);
   }
 
-  ~InstanceManager() = default;
+  ~InstanceManager() override = default;
+
+  void detectInstances(size_t initialInstanceCount)
+  {
+    // Get current instance
+    auto currentInstance = getCurrentInstance();
+
+    // Lock the pool
+    _instancePool.lock();
+
+    // Add the instance to the pool
+    _instancePool.insertInstance(currentInstance);
+
+    // Update the barrier
+    _instancePool.updateBarrier(initialInstanceCount);
+
+    // Unlock the pool
+    _instancePool.unlock();
+
+    // Wait for all the threads to add their own instance
+    _instancePool.barrier();
+
+    // Add all the instances
+    for (auto &i : _instancePool.getInstances())
+    {
+      // Skip current instance
+      if (i->getId() != currentInstance->getId()) { addInstance(i); }
+    }
+  }
 
   std::shared_ptr<HiCR::Instance> createInstanceImpl(const HiCR::InstanceTemplate instanceTemplate) override
   {
-    Instance::instanceId_t newInstanceId;
-    auto                   status = pthread_create(&newInstanceId, nullptr, launchWrapper, this);
+    // Storage for the new instance id
+    pthread_t newInstanceId;
+
+    // Launch a new pthread executing the entrypoint
+    auto status = pthread_create(&newInstanceId, nullptr, launchWrapper, this);
     if (status != 0) { HICR_THROW_RUNTIME("Could not create instance thread. Error: %d", status); }
-    return std::make_shared<Instance>(newInstanceId, _rootInstanceId);
+
+    // Add to the pool of created pthreads
+    _createdThreads.insert(newInstanceId);
+
+    // Create a new HiCR instance
+    auto instance = std::make_shared<Instance>(newInstanceId, _rootInstanceId);
+
+    // Lock the pool
+    _instancePool.lock();
+
+    // Add the new instance
+    _instancePool.insertInstance(instance);
+
+    // Update the barrier
+    _instancePool.updateBarrier(_instancePool.getInstances().size());
+
+    // Unlock
+    _instancePool.unlock();
+
+    return instance;
   }
 
   std::shared_ptr<HiCR::Instance> addInstanceImpl(Instance::instanceId_t instanceId) override { return std::make_shared<Instance>(instanceId, _rootInstanceId); }
 
   void terminateInstanceImpl(const std::shared_ptr<HiCR::Instance> instance) override
   {
-    HICR_THROW_LOGIC("The Host backend does not currently support the termination of instances during runtime");
+    // Lock the pool
+    _instancePool.lock();
+
+    // Delete instance
+    _instancePool.deleteInstance(instance);
+
+    // Unlock
+    _instancePool.unlock();
   }
 
-  void finalize() override {}
+  void finalize() override
+  {
+    for (auto threadId : _createdThreads) { pthread_join(threadId, nullptr); }
+  }
+
   void abort(int errorCode) override { exit(errorCode); }
 
   HiCR::Instance::instanceId_t getRootInstanceId() const override { return _rootInstanceId; }
@@ -63,5 +127,8 @@ class InstanceManager : public HiCR::InstanceManager
 
   HiCR::Instance::instanceId_t _rootInstanceId;
   entryPoint_t                 _entrypoint;
+  InstancePool                &_instancePool;
+
+  std::unordered_set<pthread_t> _createdThreads;
 };
 } // namespace HiCR::backend::pthreads
